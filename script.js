e585157acccb90102dcd7457b721303a6be519af
nathanim1919 <nathanim2tadele@gmail.com>
2022-08-20 21:10:16 -0700

    simple pricing page using html css and js

---
+++ script.js
@@ -0,0 +1,7 @@
+let toggle_box = document.querySelector('.box');
+let toggle = document.querySelector('.inner_box');
+
+
+toggle_box.addEventListener('click',()=>{
+   toggle.classList.toggle('monthlly');
+});
